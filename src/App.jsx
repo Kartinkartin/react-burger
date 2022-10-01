49493890f30d809788a75b05596af41185c6fbba
Catherine Kuznetcova <96156192+Kartinkartin@users.noreply.github.com>
2022-10-01 19:13:59 +0300

    Create /pages

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import {
+    ConstructorPage,
+    LoginPage,
+    ProfilePage,
+    RecoveryPassPage,
+    RegistrationPage,
+    ResetPassPage
+} from "./pages";
+
+function App() {
+
+}
+export default App;
---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import { ConstructorPage } from "./constructor";
+import { LoginPage } from "./login";
+import { ProfilePage } from "./profile";
+import { RecoveryPassPage } from "./recoveryPass";
+import { RegistrationPage } from "./registration";
+import { ResetPassPage } from "./resetPass";
+
+export {
+    ConstructorPage,
+    LoginPage,
+    ProfilePage,
+    RecoveryPassPage,
+    RegistrationPage,
+    ResetPassPage
+};
