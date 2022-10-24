aee77e88a3d64a23dceca239f46c91a9b383881e
Catherine Kuznetcova <96156192+Kartinkartin@users.noreply.github.com>
2022-10-24 21:08:11 +0300

    Finish with /feed/:id page

--- src/components/order-ingredient/order-ingredient.jsx
+++ src/components/order-ingredient/order-ingredient.jsx
@@ -9,26 +9,31 @@ import { getApiIngredients } from "../../services/selectors/selectors";
 
 
 
-export default function OrderIngredient({ id }) {
+export default function OrderIngredient({ id, counter }) {
     const ingredientsApi = useSelector(getApiIngredients);
-    const currentIngredient = ingredientsApi.find(item => item._id ===id );
+    const currentIngredient = ingredientsApi.find(item => item._id === id);
     const image = currentIngredient.image;
     return (
-        <div className={styles.container}>
-            <div 
-            className={styles.image}
-            style={{backgroundImage: `url("${image}")` }}>
+        <div className={`${styles.container}`} style={{ justifyContent: "space-between" }}>
+            <div className={styles.container}>
+                <div
+                    className={styles.image}
+                    style={{ backgroundImage: `url("${image}")` }}>
+                </div>
+                <h3 className={`${styles.name} text text_type_main-default ml-4`}>
+                    {currentIngredient.name}
+                </h3>
             </div>
-            <h3 className={`${styles.name} text text_type_main-default ml-4`}>
-                {currentIngredient.name}
-            </h3>
-            <p className="text text_type_digits-default mr-2">
-            {currentIngredient.price}
-            </p>
-            <CurrencyIcon />
-        </div>    
+            <div className={styles.container}>
+                <p className="text text_type_digits-default mr-2">
+                    {`${counter} x ${currentIngredient.price}`}
+                </p>
+                <CurrencyIcon />
+            </div>
+        </div>
     )
 }
 OrderIngredient.propTypes = {
-    
+    id: PropTypes.string.isRequired,
+    counter: PropTypes.number.isRequired
 }
