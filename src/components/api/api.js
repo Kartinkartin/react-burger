7b2424fc17110f2224c3ae9456dff09452440300
Catherine Kuznetcova <96156192+Kartinkartin@users.noreply.github.com>
2022-10-31 22:18:36 +0300

    Fix: made func fetch+checkRes

--- src/components/api/api.js
+++ src/components/api/api.js
@@ -4,6 +4,12 @@ function checkRes(res) {
   }
   return Promise.reject([`Ошибка ${res.status}`, res.json()]);
 }
+function request(url, options) {
+  return (fetch(url, {
+    options
+  })
+  .then(checkRes))
+}
 
 const config = {
   baseUrl: 'https://norma.nomoreparties.space/api',
@@ -16,16 +22,15 @@ export const wsUrl =  'wss://norma.nomoreparties.space/orders'
 
 // получение всех начальных ингредиентов
 export function getCardsRequest() {
-  return (fetch(`${config.baseUrl}/ingredients`, {
+  return request(`${config.baseUrl}/ingredients`, {
     headers: config.headers
   })
-    .then(checkRes))
 }
 
 // отправка заказа на сервер
 export function postOrderRequest(orderListId, token) {
   const order = { ingredients: orderListId };
-  return (fetch(`${config.baseUrl}/orders`, {
+  return request(`${config.baseUrl}/orders`, {
     headers: {
       ...config.headers, 
       authorization: `Bearer ${token}`
@@ -33,86 +38,78 @@ export function postOrderRequest(orderListId, token) {
     method: 'POST',
     body: JSON.stringify(order)
   })
-    .then(checkRes))
 }
 
 // сброс пароля
 export function resetPassRequest(email) {
-  return (fetch(`${config.baseUrl}/password-reset`, {
+  return request(`${config.baseUrl}/password-reset`, {
     headers: config.headers,
     method: 'POST',
     body: JSON.stringify({
       "email": email
     })
   })
-    .then(checkRes))
 }
 
 // создание нового пароля
 export function newPassRequest(newPassData) {
-  return (fetch(`${config.baseUrl}/password-reset/reset`, {
+  return request(`${config.baseUrl}/password-reset/reset`, {
     headers: config.headers,
     method: 'POST',
     body: JSON.stringify(newPassData)
   })
-    .then(checkRes))
 }
 
 // регистрация нового пользователя
 export function registerUserRequest(userData) {
-  return (fetch(`${config.baseUrl}/auth/register`, {
+  return request(`${config.baseUrl}/auth/register`, {
     headers: config.headers,
     method: 'POST',
     body: JSON.stringify(userData)
   })
-    .then(checkRes))
 }
 
 // авторизация пользователя
 export function loginUserRequest(loginData) {
-  return (fetch(`${config.baseUrl}/auth/login`, {
+  return request(`${config.baseUrl}/auth/login`, {
     headers: config.headers,
     method: 'POST',
     body: JSON.stringify(loginData)
   })
-    .then(checkRes))
 }
 
 // выход из аккаунта
 export function logoutUserRequest(logoutData) {
-  return (fetch(`${config.baseUrl}/auth/logout`, {
+  return request(`${config.baseUrl}/auth/logout`, {
     headers: config.headers,
     method: 'POST',
     body: JSON.stringify(logoutData)
   })
-    .then(checkRes))
 }
 
 // обновление токена
 export function refreshTokenRequest(tokenData) {
-  return (fetch(`${config.baseUrl}/auth/token`, {
+  return request(`${config.baseUrl}/auth/token`, {
     headers: config.headers,
     method: 'POST',
     body: JSON.stringify(tokenData)
   })
-    .then(checkRes))
 }
 
 // получение информации профиля пользователя
 export function getUserRequest(token) {
-  return (fetch(`${config.baseUrl}/auth/user`, {
+  return request(`${config.baseUrl}/auth/user`, {
     headers: {
       ...config.headers,
       authorization: `Bearer ${token}`
     },
     method: 'GET',
   })
-    .then(checkRes))
 }
 
 // изменение данных профиля пользователя, пароль тоже меняется
 export function changeUserDataRequest(token, userData) {
-  return (fetch(`${config.baseUrl}/auth/user`, {
+  return request(`${config.baseUrl}/auth/user`, {
     headers: {
       ...config.headers,
       authorization: `Bearer ${token}`
@@ -120,5 +117,4 @@ export function changeUserDataRequest(token, userData) {
     method: 'PATCH',
     body: JSON.stringify(userData)
   })
-    .then(checkRes))
 }
