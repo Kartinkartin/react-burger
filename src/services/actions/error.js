5721358be4765462315cbc3873a52caeb1f0d434
Catherine Kuznetcova <96156192+Kartinkartin@users.noreply.github.com>
2022-11-13 15:13:58 +0300

    Type actions types

--- src/services/actions/error.js
+++
@@ -1,2 +0,0 @@
-export const ERROR_SET = 'ERROR_SET';
-export const ERROR_RESET = 'ERROR_RESET';
--- src/services/actions/login.js
+++
@@ -1,4 +0,0 @@
-export const USER_RESET = 'USER_RESET';
-export const USER_SET = 'USER_SET';
-export const USER_REFRESH = 'USER_REFRESH';
-export const USER_CHANGE_DATA = 'USER_CHANGE_DATA'
