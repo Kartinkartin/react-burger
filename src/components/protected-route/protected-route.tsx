a3616b26a7d88662ad77bcf78b94d1b5922f55d3
Catherine Kuznetcova <96156192+Kartinkartin@users.noreply.github.com>
2022-11-18 22:43:03 +0300

    Type Router

--- src/components/protected-route/protected-route.tsx
+++ src/components/protected-route/protected-route.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import { useLocation, Route, Redirect } from 'react-router-dom';
 
-export function ProtectedRoute({ loggedUser = false, children, ...rest }) {
+type TRouteProps = {
+  loggedUser:boolean, 
+  children: any, 
+  rest: any
+}
+export const ProtectedRoute = ({ loggedUser = false, children, ...rest }: TRouteProps) => {
   const location = useLocation();
   const wasLogged = document.cookie.includes('refreshToken');
   /* защита роутинга для страниц входа, регистрации, восстановления пароля  */
