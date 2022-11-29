6d64f5d1b2466f8ff199fd936c9146f363f732a9
Catherine Kuznetcova <96156192+Kartinkartin@users.noreply.github.com>
2022-11-29 22:27:39 +0300

    Exchange any, add conditions

--- src/components/order-detail/order-detail.tsx
+++ src/components/order-detail/order-detail.tsx
@@ -1,10 +1,9 @@
-import React, { FunctionComponent }  from "react";
+import React, { FunctionComponent } from "react";
 import { useParams } from 'react-router-dom';
-import { getApiIngredients,getWSOrders } from "../../services/selectors/selectors";
+import { getApiIngredients, getWSOrders } from "../../services/selectors/selectors";
 import { OrderIngredient } from '../order-ingredient/order-ingredient';
 import { CurrencyIcon } from '@ya.praktikum/react-developer-burger-ui-components';
 import styles from "./order-detail.module.css";
-import { TIngredient } from "../../services/types/data";
 import { useSelector } from "../../services/hooks/hooks";
 
 export const OrdertDetail: FunctionComponent = () => {
@@ -16,51 +15,52 @@ export const OrdertDetail: FunctionComponent = () => {
     ) : null;
     const ingredientsApi = useSelector(getApiIngredients);
     if (currentOrder) {
-        
-    }
-    const price = currentOrder ? currentOrder.ingredients.reduce((price: number, current: string) => {
-        const currentPrice = ingredientsApi.find((el: TIngredient) => el._id === current)!.price;
-        return price + currentPrice
-    }, 0) : 0;
-    const moment = require('moment'); // библиотека uuid для форматирования даты  
-    require('moment/locale/ru');
-    const orderDate = currentOrder ? moment(currentOrder.createdAt).calendar() : undefined;
-    return (
-        ingredientsList && ingredientsApi && (
-            <div className={styles.container}>
-                <h2 className={`${styles.number} text text_type_digits-default pb-10`}>
-                    {`#${currentOrder!.number}`}
-                </h2>
-                <h1 className='text text_type_main-medium pb-3'>
-                    {currentOrder!.name}
-                </h1>
-                <p className={`${styles.status} text text_type_main-default`}>
-                    {currentOrder!.status === 'done' ? <>Выполнен</> : <>В работе</>}
-                </p>
-                <p className='text text_type_main-medium pt-15 pb-6'>Состав:</p>
-                <div className={`${styles.ingredients} pr-6`}>
-                    {ingredientsList.map((item: string) => {
-                        const counter = currentOrder!.ingredients.filter((el: string) => el === item).length;
-                        return (
-                            <OrderIngredient
-                                id={item}
-                                counter={counter}
-                                key={item} />
-                        )
-                    })}
-                </div>
-                <div className={`${styles.service_info} pt-10`}>
-                    <p className={`${styles.date} text text_type_main-default`} >
-                        {orderDate}
+        const price = currentOrder ?
+            currentOrder.ingredients.reduce((price: number, current: string) => {
+                const currentPrice = ingredientsApi.find((el) => el._id === current)!.price;
+                return price + currentPrice
+            }, 0) : 0;
+        const moment = require('moment'); // библиотека uuid для форматирования даты  
+        require('moment/locale/ru');
+        const orderDate = currentOrder ? moment(currentOrder.createdAt).calendar() : undefined;
+        return (
+            ingredientsList && ingredientsApi && currentOrder && (
+                <div className={styles.container}>
+                    <h2 className={`${styles.number} text text_type_digits-default pb-10`}>
+                        {currentOrder.number}
+                    </h2>
+                    <h1 className='text text_type_main-medium pb-3'>
+                        {currentOrder.name}
+                    </h1>
+                    <p className={`${styles.status} text text_type_main-default`}>
+                        {currentOrder.status === 'done' ? <>Выполнен</> : <>В работе</>}
                     </p>
-                    <p className={styles.price_container}>
-                        <span className={`text text_type_digits-default pr-2`}>
-                            {price}
-                        </span>
-                        <CurrencyIcon type='primary' />
-                    </p>
+                    <p className='text text_type_main-medium pt-15 pb-6'>Состав:</p>
+                    <div className={`${styles.ingredients} pr-6`}>
+                        {ingredientsList.map((item: string) => {
+                            const counter = currentOrder.ingredients.filter((el: string) => el === item).length;
+                            return (
+                                <OrderIngredient
+                                    id={item}
+                                    counter={counter}
+                                    key={item} />
+                            )
+                        })}
+                    </div>
+                    <div className={`${styles.service_info} pt-10`}>
+                        <p className={`${styles.date} text text_type_main-default`} >
+                            {orderDate}
+                        </p>
+                        <p className={styles.price_container}>
+                            <span className={`text text_type_digits-default pr-2`}>
+                                {price}
+                            </span>
+                            <CurrencyIcon type='primary' />
+                        </p>
+                    </div>
                 </div>
-            </div>
+            )
         )
-    )
+    }
+    return null
 }
--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,7 @@ import reportWebVitals from './reportWebVitals';
 
 import { store } from './services/store';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById("root")!);
 
 root.render(
     <Router>
